fd3cc0ae292d52861796de19c51625a5a46c52fa
Mickaël Véril <mveril@irsamc.ups-tlse.fr>
2019-08-28 11:59:18 +0200

    Handle .dat format and delete json

--- scripts/data.js
+++ scripts/data.js
@@ -1,21 +1,122 @@
-function getDataFromFile(file,fn) {
-  var result = null;
-  var xmlhttp = new XMLHttpRequest();
-  xmlhttp
-  xmlhttp.open("GET",getFullDataPath(file), true);
-  xmlhttp.onreadystatechange = function() { 
-    if (this.readyState == 4 && this.status == 200) {//when a good response is given do this
-
-        var data = JSON.parse(this.responseText); // convert the response to a json object
-        fn(data)
-    }
+class code {
+  constructor(name,version){
+    this.name=name
+    this.version=version
   }
-  xmlhttp.send();
+}
+class basis {
+  constructor(name,type){
+    this.name=name
+    this.type=type
+  }
 }
 
-function getDoiUrl(doi) {
-  return 'https://doi.org/'+doi
+class state{
+  constructor(number,multiplicity,symetry){
+    this.number=number;
+    this.multiplicity=multiplicity;
+    this.symetry=symetry;
+  }
 }
-function getFullDataPath(path) {
-  return 'data/'+path
+class doi{
+  constructor(doistring){
+    this.string=doistring
+  }
+  get url() {
+    return 'https://doi.org/'+doi 
+  }
+}
+class excitation{
+  constructor(start,end,Evert){
+    this.start=start;
+    this.end=end;
+    this.Evert=Evert;
+  }
+}
+
+class data {
+  constructor(){
+    this.title="";
+    this.code=null;
+    this.basis=null;
+    this.doi=null;
+    this.excitations=[];
+  }
+  static load(file,fn) {
+    var req = new XMLHttpRequest();
+    req.open("GET",getFullDataPath(file), true);
+    req.onreadystatechange = function() { 
+      if (req.readyState == 4 && req.status == 200) {//when a good response is given do this
+        var text = req.responseText;
+        fn(data.loadstring(text));
+      }
+    }
+    req.send();
+  }
+  static loadstring(text) {
+    // for each line with metadata
+    var ismetaArea=true;
+    //metadata RegExp (start with #; maybe somme spaces; : ; maybe somme space; datas)
+    var meta=/^#\s*([A-Za-z]+)\s*:\s*(.*)$/;
+    var dat=new data();
+    function readmeta(line){
+      // get key value
+      var match=line.match(meta);
+      // normalize key to lower
+      var key=match[1].toLowerCase();
+      //if data has value
+      if(match.length==3 && match[2]) {
+        var val=match[2];
+        switch(key) {
+          case "title":
+            dat.title=val;
+            break;
+          case "code":
+            var vals=val.split(",")
+            if(vals.length>=2){
+              dat.code=new code(vals[0],vals[1]);
+            } else {
+              dat.code=new code(vals[0],null);
+            }
+            break;
+          case "basis":
+            var vals=val.split(",")
+            if(vals.length>=2){
+              dat.basis=new basis(vals[0],vals[1])
+            } else {
+              dat.basis=new basis(vals[0],null);
+            }
+            break;
+          case "doi":
+            dat.doi=new doi(val);
+            break;
+        }
+      }
+    }
+    function readrow(line){
+      var vals=line.split(/\s+/);             
+      var start=new state(vals[0],vals[1],vals[2]);
+      var end=new state(vals[3],vals[4],vals[5]);
+      var ex=new excitation(start,end,vals[6]);
+      dat.excitations.push(ex);
+    };
+
+    text.split("\n").forEach(function(line) { 
+      //if it's not empty line
+      line=line.trim();
+      if (line){
+        //if # may be metadata or comment
+        if (line.charAt(0)=="#") {
+          //if it's metadata
+          if(ismetaArea && meta.test(line)) {
+            readmeta(line);
+          }				
+        } else { //else its row
+          ismetaArea=false;
+          readrow(line);
+        }
+      }
+    });
+    return dat
+  }
 }
---
+++ scripts/getFullDataPath.js
@@ -0,0 +1,3 @@
+function getFullDataPath(path) {
+  return 'data/' + path;
+}
