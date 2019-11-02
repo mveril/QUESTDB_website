9cd212ab33c55a69b73bf0766d7a0c197de52eba
Mickaël Véril <mveril@irsamc.ups-tlse.fr>
2019-11-02 15:53:53 +0100

    Performance improvement using the new DOICache class

---
+++ static/js/DOICache.js
@@ -0,0 +1,36 @@
+class DOICache{
+  constructor(){
+  this._doimap=new Map()
+  this._Cite=require("citation-js")
+  }
+  clear(){
+    this._doimap.clear()
+  }
+  delete(doi){
+    return this._doimap.delete(doi)
+  }
+  forEach(callbackfn, thisArg=None){
+    return this._doimap.forEach(callbackfn,thisArg)
+  }
+  get(doi){
+    return this._doimap.get(doi)
+  }
+  has(doi){
+    return this._doimap.has(doi)
+  }
+  async add(doi){
+    if(!this.has(doi)){
+      const publi=await this._Cite.async(doi)
+      this._doimap.set(doi,publi)
+    }
+  }
+  async addRange(dois){
+    const set=new Set(dois)
+    for(const doi of set){
+     await this.add(doi)
+    }
+  }
+  get size(){
+    return this._doimap.size()
+  }
+}
