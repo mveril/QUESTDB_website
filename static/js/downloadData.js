5c76b08c2dc9024032d84f4953f3faeaec84db22
Mickaël Véril <mveril@irsamc.ups-tlse.fr>
2019-11-09 17:40:37 +0100

    Fix download Base64string data

---
+++ static/js/downloadData.js
@@ -0,0 +1,11 @@
+function downloadData(data,fileName) {
+  const url=window.URL.createObjectURL(data)
+  const a = document.createElement('a');
+  a.style.display = 'none';
+  a.href = url;
+  a.download = fileName;
+  document.body.appendChild(a);
+  a.click();
+  window.URL.revokeObjectURL(url);
+  document.body.removeChild(a)
+}
