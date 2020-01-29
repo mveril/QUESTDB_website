47487c4d61b81233b77febc7e7b6b80ead476b38
Mickaël Véril <mveril@irsamc.ups-tlse.fr>
2020-01-29 17:36:50 +0100

    Modernize option selection

--- static/js/selectSelectAll.js
+++ static/js/selectSelectAll.js
@@ -2,7 +2,7 @@ function selectSelectAll_click(e) {
   const ctrl = $(e.target).next()
   if (ctrl.prop('type') == 'select-multiple') {
     ctrl.find("option").each(function () {
-      $(this).attr('selected', 'selected');
+      $(this).prop('selected', true);
       ctrl.trigger("change")
     });
   }
