9bc755fed5b8f424e8f99a5eb29c85801e35e73b
Mickaël Véril <mveril@irsamc.ups-tlse.fr>
2020-02-05 20:03:51 +0100

    Print error message when Latex cannot be parsed for state

--- tools/lib/data.py
+++ tools/lib/data.py
@@ -44,7 +44,12 @@ class dataFileBase(object):
       math=TexState.find("$")
       lst=list(math.contents)
       mystr=str(lst[0])
-      mathsoup=TexSoup(mystr)
+      mathsoup=None
+      try:
+        mathsoup=TexSoup(mystr)
+      except:
+        print(f"Error when parsing latex state: {mystr}")
+        exit(-1)
       newCommand.runAll(mathsoup,commands)
       st=str(mathsoup)
       m=re.match(r"^\^(?P<multiplicity>\d)(?P<symm>[^\s\[(]*)\s*(?:\[(?:\\mathrm{)?(?P<special>\w)(?:})\])?\s*(:?\((?P<type>[^\)]*)\))?",st)
