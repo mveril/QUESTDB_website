e75a1de5427b20f35625e04c8339590ba195cddb
Mickaël Véril <mveril@irsamc.ups-tlse.fr>
2020-02-10 16:29:19 +0100

    Delete usage of zpe folder

--- static/js/loadAllData.js
+++ static/js/loadAllData.js
@@ -2,7 +2,6 @@ async function loadAllData() {
   dic = {
     abs: [],
     fluo: [],
-    ZPE: []
   };
   dic.abs = await Promise.all(getAbsFilesName().map((f) => VertDataFile.loadAsync(f,VertExcitationKinds.Absorbtion)))
   dic.fluo = await Promise.all(getFluoFilesName().map((f) => VertDataFile.loadAsync(f,VertExcitationKinds.Fluorescence)))
