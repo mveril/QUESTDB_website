62acffa870bc2b04bc7d55d0483ff637741e2b7f
Mickaël Véril <mveril@irsamc.ups-tlse.fr>
2020-08-20 15:28:06 +0200

    New publis handling with sets which support multiple publications

---
+++ static/js/PubliDB.js
@@ -0,0 +1,34 @@
+class publiDB {
+  static async loadAsync() {
+    var db = new publiDB
+    const text = await getTextFromFileUrlAsync("/data/publis/index.yaml")
+    const myYaml = jsyaml.load(text);
+    db.sets = ((myYaml.sets === null) ? new Map() : new Map(Object.entries(myYaml.sets)));
+    db.others = ((myYaml.others === null) ? new Map() : new Map(Object.entries(myYaml.others)));
+    return db
+  }
+  static get UnknowSetName() {
+    return "Unknow set"
+  }
+  findNameFromSet(set, supportUnknow = false) {
+    if (supportUnknow && JSON.stringify(set) === JSON.stringify([""])) {
+      return publiDB.UnknowSetName
+    }
+    for (let [setname, dois] of this.sets.entries()) {
+      if (JSON.stringify(set.sort()) === JSON.stringify(dois.sort())) {
+        return setname
+      }
+    }
+    return null
+  }
+  findSetNameFromDOI(doi, supportUnknow = false) {
+    for (let [setname, dois] of this.sets.entries()) {
+      if (dois.includes(doi))
+        return setname
+    }
+    if (supportUnknow && doi === "") {
+      return UnknowSetName
+    }
+    return null
+  }
+}
