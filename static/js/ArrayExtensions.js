1119c6c9e517e33f1049f63d5476ee7cfe4608cb
Mickaël Véril <mveril@irsamc.ups-tlse.fr>
2020-09-24 16:56:51 +0200

    Add findAllIndexes Array extension

--- static/js/ArrayExtensions.js
+++ static/js/ArrayExtensions.js
@@ -6,4 +6,13 @@ if (!Array.prototype.findAsync) {
       }
     }
   }
+  if (!Array.prototype.findAllIndexes) {
+    Array.prototype.findAllIndexes = function (Callback) {
+      this.reduce(function(a, e, i) {
+        if (Callback(e))
+          a.push(i)
+        return a
+    }, [])
+    }
+  }
 }
