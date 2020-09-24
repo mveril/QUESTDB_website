6ad145ba0e4ffd660572daa15d7bf51a8d0229c7
Mickaël Véril <mveril@irsamc.ups-tlse.fr>
2020-09-24 16:53:11 +0200

    Remove unnescesary function name for ArrayExtensions

--- static/js/ArrayExtensions.js
+++ static/js/ArrayExtensions.js
@@ -1,5 +1,5 @@
 if (!Array.prototype.findAsync) {
-  Array.prototype.findAsync = async function findAsync(asyncCallback) {
+  Array.prototype.findAsync = async function (asyncCallback) {
     for (const item of this) {
       if (await asyncCallback(item)) {
         return item
