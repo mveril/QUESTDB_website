6eb8717f55aade5b6405ebd482cdb4194c40c23d
Mickaël Véril <mveril@irsamc.ups-tlse.fr>
2020-11-01 16:57:16 +0100

    Save indexDB to window.indexDB automaticaly to avoid re-created it

--- static/js/indexDB.js
+++ static/js/indexDB.js
@@ -1,11 +1,15 @@
 class indexDB {
   static async loadAsync() {
+    if (window.indexDB) {
+      return window.indexDB
+    }
     var db = new indexDB
     const maxAge= (DebugMode.Enabled,0,600)
     const text = await getTextFromFileUrlAsync("/data/index.yaml",{"Cache-Control":`max-age=${maxAge}`})
     const myYaml = jsyaml.load(text);
     db.sets = ((myYaml.sets === null) ? new Map() : new Map(Object.entries(myYaml.sets)));
     db.others = ((myYaml.others === null) ? [] : Array.from(myYaml.others));
+    window.indexDB=db
     return db
   }
 }
