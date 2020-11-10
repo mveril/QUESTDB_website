e772846992b0ae7e2944abb8b293bbdadc0c4875
Mickaël Véril <mveril@irsamc.ups-tlse.fr>
2020-11-10 17:40:14 +0100

    Replace metarecover bash script by a more flexible python version in order to use python for each tools

---
+++ tools/metarecover.py
@@ -0,0 +1,25 @@
+from git import Repo,Git
+import io
+r=Repo(path=".")
+l=[item for item in r.index.diff(None) if item.a_path.endswith(".dat") and item.change_type=="M"]
+len(l)
+for i in l:
+  print(i.a_path)
+  g=Git(Repo.git_dir)
+  with io.StringIO(g.execute(["git" ,"--no-pager", "show", f"HEAD~1:{i.a_path}"])) as old:
+    with open(i.a_path,mode="r+") as new:
+      with io.StringIO(new.read()) as copy:
+        new.seek(0)
+        line=old.readline()
+        while line and line.startswith("#"):
+          new.write(line)
+          line=old.readline()
+        line=copy.readline()
+        end=False
+        while line:
+          if not end and not line.startswith("#"):
+            end=True
+          if end:
+            new.write(line)
+          line=copy.readline()
+      new.truncate()
