14ae7312fc29f7e29a496187e3a2de6cbc894bb3
Mickaël Véril <mveril@irsamc.ups-tlse.fr>
2021-02-17 16:39:54 +0100

    Improvements for publications

--- static/js/pubUtils.js
+++ static/js/pubUtils.js
@@ -1,6 +1,27 @@
-class pubUtils{
-  static getIssuedDate(publi) {
-    const parts=publi.issued["date-parts"][0]
-    return new Date(Date.UTC(parts[0], parts[1] - 1, parts[2]))
+class pubUtils {
+  static parseDate(date) {
+    if (date["date-time"]) {
+      return Date.parse(date["date-time"])
+    }
+    if (date.timestamp) {
+      return Date(date.timestamp)
+    }
+    const parts=date["date-parts"][0]
+    const [year, month, day] = date['date-parts'][0]
+    if (day) {
+      return new Date(Date.UTC(year, month - 1, day))
+    } else if (month) {
+      return new Date(Date.UTC(year, month - 1))
+    } else {
+      return new Date(Date.UTC(year))
+    }
+  }
+  static bestDate(publi) {
+    if (publi.issued != null) {
+      return {type:"issued", dateInfo:publi.issued}
+    }
+    else {
+      return {type:"created", dateInfo:publi.created}
+    }
   }
 }
