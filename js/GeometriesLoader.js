2f84e5bd1bdef4bf13d692ea5f958f174a6d3cc7
mveril <mveril@users.noreply.github.com>
2021-11-09 09:53:11 +0000

    deploy: 8d4f5c40123a19a73e019bf1d26aed385d27c97c

--- js/GeometriesLoader.js
+++ js/GeometriesLoader.js
@@ -2,13 +2,13 @@ class GeometriesLoader{
   static async loadForAsync(array){
     var xyzs=[]
     for (const item of array) {
-      const mymol=mhchemCE.extract(item.molecule).toLowerCase().replace(" ","_")
-      const myset=item.set.split("#").join("")
+      const mymol=moleculeFormater.toFileName(item.molecule)
+      const myset=item.set.replace('#','')
       try {
         const mol=await Geometry.loadXYZAsync(`/${myset}/${mymol}.xyz`)
         xyzs.push(mol)
       } catch (error) {
-        console.error("Geometry not found",item)
+        console.error("Geometry not found",item,error)
       }
     }
     return xyzs.flat()
